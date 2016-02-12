6963340087c562f79bf7ba245fe3cae5f9fcfd7a
Tom Johns <tommyskies@hotmail.com>
2016-02-12 13:39:57 -0700

    update to remove .git files

--- lib/internet_wisdom.rb
+++ lib/internet_wisdom.rb
@@ -3,12 +3,18 @@ require "pry"
 require "nokogiri"
 require "launchy"
 
+require "internet_wisdom/cli"
+require "internet_wisdom/post"
+require "internet_wisdom/site"
+require "internet_wisdom/site_manager"
+require "internet_wisdom/version"
+
 # requires all files recursively inside a directory from current dir
 # @param _dir can be relative path like '/lib' or "../lib"
-def require_all(_dir)
-    Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), _dir)) + "/**/*.rb"].each do |file|
-        require file
-    end
-end
+#def require_all(_dir)
+    #Dir[File.expand_path(File.join(File.dirname(File.absolute_path(__FILE__)), _dir)) + "/**/*.rb"].each do |file|
+    #    require file
+    #end
+#end
 
-require_all("../lib")
+#require_all("../lib")
